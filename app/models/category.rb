ce8b50b1f4ebc177eaad12f223fa2b9bd812a3b1
Ben Lewis <bennlewis@gmail.com>
2013-11-04 19:00:25 -0700

    added specs for ingredient and category associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
   validates :title, presence: true
 
+  has_many :recipe_categories
+  has_many :recipes, through: :recipe_categories
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Ingredient do
   it { should validate_presence_of :title }
+  it { should have_many :recipes }
 end
