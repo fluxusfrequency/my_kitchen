671ecfc85a9cb40ee2510abf91f0d48ac556b91e
Ben Lewis <bennlewis@gmail.com>
2013-11-04 18:13:53 -0700

    setup dependencies

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.1'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'jbuilder', '~> 1.2'
+gem 'rspec-rails'
+gem 'unicorn'
+
+group :development, :test do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'pry'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyKitchen::Application.config.secret_key_base = 'ce01d88a9ac029b4614b8c0539a2a717d27b75291032ed75b860a953e6d21cd026bbf73ec21ab42a4e034e53d4be6d1e92e28aa4140e24207b33ff9b7f7c9a2d'
